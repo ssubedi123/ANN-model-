c75d20baf0dba4ba4cb1c5ba1303c5e83790696a
Sudesh subedi <47485180+ssubedi123@users.noreply.github.com>
2020-12-05 18:09:21 -0600

    Add files via upload

---
+++ untitled13.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 10 22:37:51 2020
+
+@author: sudesh
+"""
+import os
+os.getcwd(
+
+
+ import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
